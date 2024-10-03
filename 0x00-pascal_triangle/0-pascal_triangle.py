86a8524bb71e5548abe91d6bd5c7651ab8117c0a
Maryam <morrryyyy21@gmail.com>
2024-10-03 19:04:09 +0100

    pascal's triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""This is the 0-pascal's triangle module."""
+
+
+def pascal_triangle(n):
+    triangle = []
+    if n <= 0:
+        return []
+    i = 0
+    for i in range(n):
+        row = []
+        j = 0
+        for j in range (i + 1):
+            if j == 0 or j == i:
+                row.append(1)
+            else:
+                element = triangle[i - 1][j] + triangle[i - 1][j - 1]
+                row.append(element)
+        triangle.append(row)
+    return triangle
---
+++ 0x00-pascal_triangle/README.md
@@ -0,0 +1,1 @@
+an implementation of the pascal's triangle algorithm using python
