4eb248d5e0a416a26b71fa0dbedf6ed28356f854
Maryam <morrryyyy21@gmail.com>
2024-10-03 19:04:44 +0100

    pascal's triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -10,7 +10,7 @@ def pascal_triangle(n):
     for i in range(n):
         row = []
         j = 0
-        for j in range (i + 1):
+        for j in range(i + 1):
             if j == 0 or j == i:
                 row.append(1)
             else:
